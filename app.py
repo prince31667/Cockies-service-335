63aee44ff13f38a9861dfffcdfc6b1f1107e64ae
prince31667 <princeeroy316@gmail.com>
2025-03-23 20:07:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,6 +128,6 @@ def send_message():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
